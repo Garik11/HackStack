e289674a8c44893b841da545d5a11c1a0ffeecb4
Garik11 <garik.irk@mail.ru>
2023-09-29 20:40:11 +0300

    StackHash get better

--- StackHash.cpp
+++ StackHash.cpp
@@ -13,14 +13,25 @@ HASH StackGetStructHash(Stack *stk, uint64_t seed /* = 5461216 */){
 }
 
 HASH StackGetDataHash(Stack *stk, uint64_t seed /* = 5461216 */){
+    if(stk->data == NULL)
+        return 0;
     #ifdef USE_CALIBRI
-        return (stk->data == NULL) ? 0 :
-        HashNoCommutate(stk->data, stk->capaticy * sizeof(Elem_t) + 2 * sizeof(Calibri), seed,
-            0xF00, 0xc2b2ae35, 0x9);
+        return HashNoCommutate( stk->data, 
+                                stk->capaticy * sizeof(Elem_t) + 2 * sizeof(Calibri), 
+                                seed, 
+                                0xF00, 
+                                0xc2b2ae35, 
+                                0x9
+                            );
+        
     #else
-        return (stk->data == NULL) ? 0 :
-        HashNoCommutate(stk->data, stk->capaticy * sizeof(Elem_t), seed,
-            0xF00, 0xc2b2ae35, 0x9);
+        return HashNoCommutate( stk->data, 
+                                stk->capaticy * sizeof(Elem_t), 
+                                seed, 
+                                0xF00, 
+                                0xc2b2ae35, 
+                                0x9
+                            );
     #endif
 }
 
